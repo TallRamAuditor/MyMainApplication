8b6b48caa53293c5cab50899c70077f8c87d4003
Ilya Volegov <heroilya12@gmail.com>
2023-10-27 13:46:46 +0500

    Tochka4_lab3

--- app/src/main/java/com/example/mymainapplication/MainActivityLab4_2.java
+++ app/src/main/java/com/example/mymainapplication/MainActivityLab4_2.java
@@ -4,16 +4,43 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivityLab4_2 extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_main);
+        setContentView(R.layout.lab4_3_activity_main2);
     }
-    public void onClick(View view) {
+    /*public void onClick(View view) {
         Intent intent = new Intent(this, SecondMainActivity.class);
         startActivity(intent);
+    }*/
+   /* public void onClick(View v) {
+        EditText nameText = findViewById(R.id.name);
+        EditText companyText = findViewById(R.id.company);
+        EditText ageText = findViewById(R.id.age);
+        String name = nameText.getText().toString();
+        String company = companyText.getText().toString();
+        int age = Integer.parseInt(ageText.getText().toString());
+        Intent intent = new Intent(this, SecondMainActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("company", company);
+        intent.putExtra("age", age);
+        startActivity(intent);
+    }*/
+    public void onClick(View v) {
+        EditText nameText = findViewById(R.id.name);
+        EditText companyText = findViewById(R.id.company);
+        EditText ageText = findViewById(R.id.age);
+        String name = nameText.getText().toString();
+        String company = companyText.getText().toString();
+        int age = Integer.parseInt(ageText.getText().toString());
+        User user = new User(name, company, age);
+        Intent intent = new Intent(this, SecondMainActivity.class);
+        intent.putExtra(User.class.getSimpleName(), user);
+        startActivity(intent);
     }
+
 }
 
--- app/src/main/java/com/example/mymainapplication/SecondMainActivity.java
+++ app/src/main/java/com/example/mymainapplication/SecondMainActivity.java
@@ -11,10 +11,36 @@ public class SecondMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_main);
-        TextView textView = new TextView(this);
-        textView.setTextSize(20);
+        /*TextView textView = new TextView(this);
+        textView.setTextSize(26);
         textView.setPadding(16, 16, 16, 16);
-        textView.setText("SecondActivity");
+        Bundle arguments = getIntent().getExtras();
+        if(arguments!=null){
+            String name = arguments.get("name").toString();
+            String company = arguments.getString("company");
+            int age = arguments.getInt("age");
+            textView.setText("Name: " + name + "\nCompany: " +
+                    company +
+                    "\nAge: " + age);
+        }
+        setContentView(textView);*/
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16, 16, 16);
+        Bundle arguments = getIntent().getExtras();
+        User user;
+        if(arguments!=null){
+            user = (User)arguments.getSerializable
+                    (User.class.getSimpleName());
+            textView.setText("Name: " + user.getName() +
+                    "\nCompany: " + user.getCompany() +
+                    "\nAge: " + String.valueOf(user.getAge()));
+        }
+
         setContentView(textView);
-    }
+
+
+
+}
 }
